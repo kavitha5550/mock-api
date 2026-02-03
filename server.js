ed41178b88225073926c097c57b364f1335bba76
Kavitha <kavithabme2003@gmail.com>
2026-02-03 11:34:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log("Mock API running on port " + PORT);
+});
